85b427dcb1f94d0c80fcf65f42628184561e6c0b
sethu p sanil <sethupsanil666@gmail.com>
2023-03-08 09:36:48 +0530

    feat : click handler added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import  ButtonAtom  from '@atom/ButtonAtom.vue'
 import { BUTTON_ROUNDED, BUTTON_VARIANTS } from '@/enums/ButtonEnums';
+const buttonClickHandler =(event:MouseEvent)=>{ console.log(event);
+}
 </script>
 
 <template>
@@ -8,7 +10,7 @@ import { BUTTON_ROUNDED, BUTTON_VARIANTS } from '@/enums/ButtonEnums';
   <button-atom :variant="BUTTON_VARIANTS.flat">{{BUTTON_VARIANTS.flat}}</button-atom>
   <button-atom :variant="BUTTON_VARIANTS.outlined">{{BUTTON_VARIANTS.outlined}}</button-atom>
   <button-atom :variant="BUTTON_VARIANTS.text">{{BUTTON_VARIANTS.text}}</button-atom>
-  <button-atom :rounded="BUTTON_ROUNDED.pill" :color="'#f00'" :block="true">{{BUTTON_ROUNDED.pill}}</button-atom>
+  <button-atom :rounded="BUTTON_ROUNDED.pill" @click="buttonClickHandler" :color="'#f00'" :block="true">{{BUTTON_ROUNDED.pill}}</button-atom>
   <button-atom>new</button-atom>
   <RouterView />
 </template>
