cc032f1cea17624f6ab6582de594b4712a3a6e8f
sethu p sanil <sethupsanil666@gmail.com>
2023-03-10 13:15:20 +0530

    plugin added

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
 import './assets/main.css'
 
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/dist/vuetify.css'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import App from './App.vue'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
+registerPlugins(app)
 
 app.mount('#app')
